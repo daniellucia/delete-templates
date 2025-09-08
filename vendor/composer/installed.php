6127d442239563dd8144134d5af7a2680619fb09
Daniel Lúcia <dlucia@refineria.es>
2025-09-08 12:39:33 +0200

    Cambiamos logica para usar composer y la autocarga

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'daniel-lucia/delete-templates',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9573e9b166a73cba98e537518287e56532e17fab',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'daniel-lucia/delete-templates' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9573e9b166a73cba98e537518287e56532e17fab',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
