cf16a4d7c0a52facc44d4a6d5ce22cfd2469e915
Daniel Lúcia <daniellucia84@gmail.com>
2025-09-19 12:21:22 +0200

    Fix

--- delete-templates.php
+++ delete-templates.php
@@ -52,7 +52,7 @@ require_once $autoload_file;
 define('DELETE_THEMES_VERSION', '2.5.0');
 define('DELETE_THEMES_FILE', __FILE__);
 
-add_action('init', function () {
+add_action('plugins_loaded', function () {
 
     load_plugin_textdomain('delete-templates', false, dirname(plugin_basename(__FILE__)) . '/languages');
 
